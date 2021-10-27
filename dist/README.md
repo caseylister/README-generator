9a7fb916ab8876cb705af41c512114dfd542d285
caseylister <caseynlister@gmail.com>
2021-10-27 14:11:27 -0400

    initial files with questions array

---
+++ dist/README.md
@@ -0,0 +1,44 @@
+# README Generator
+
+  ## Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  
+  * [Contribution](#contribution)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#Contents)
+
+  This project is designed to make creating README files a simpler process for developers. Users will not need to worry about markdown syntax and can focus simply on their content. This application will provide a series of questions that once answered, will be generated into a professional README.
+
+  ## [Installation](#Contents)
+
+  From this repository, download the zip file and add it to a directory of your choice. You also have the option of cloning the repository to a directory of your choice. The video below demonstrates both of these circumstances. Once you have the files on your computer, open VS Code or any other coding editor. You can then run the app from the root directory of these files by running node index.js in your terminal.
+
+  ## [Usage](#Contents)
+
+  After running node index.js, answer the prompts that appear in your terminal thoroughly and completely. Once finished, you should see your responses console logged in the terminal along with a message reading 'File created!'. From there, you can navigate to the /dist folder in the directory and your generated README file will be there titled README.md. 
+
+  For more information on Markdown syntax and how to add screenshots, visit the following website:
+  [Markdown Guide](https://www.markdownguide.org/)
+
+   
+
+  ## [Contributions](#Contents)
+
+  Thank you for your interest in assisting with my project! At this time, I will not be accepting contributions.
+
+  ## [Tests](#Contents)
+
+  There are no tests on this application.
+
+  ## [Questions](#Contents)
+
+  You can contact me by clicking the following links!
+
+  [Email: caseynlister@gmail.com](mailto:caseynlister@gmail.com)
+
+  [GitHub](https://github.com/caseylister)
---
+++ index.js
@@ -0,0 +1,138 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input
+const questions = [
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your title!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'username',
+            message: 'What is your GitHub username? (Required)',
+            validate: nameInput => {
+                if(nameInput){
+                    return true;
+                } else{
+                    console.log("Please enter your GitHub username!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address? (Required)',
+            validate: emailInput => {
+                if(emailInput){
+                    return true;
+                } else{
+                    console.log('Please enter your email!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please provide a description of your project. (Required)',
+            validate: description => {
+                if(description){
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Provide a step-by-step instructions on how to run your project. (Required)',
+            validate: installation => {
+                if(installation){
+                    return true;
+                } else {
+                    console.log('Please enter installation instructions!')
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Provide some instructions and examples for use. (Required)',
+            validate: usage => {
+                if(usage){
+                    return true;
+                } else{
+                    console.log('Please enter your instructions for use!')
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "confirmContribution",
+            message:
+              'Would you like to allow others to contribute to your project?',
+            default: true,
+        },
+        {
+            type: 'input',
+            name: 'contribution',
+            message: 'Please provide guidelines on how to contribute to your project. (Required)',
+            when: ({ confirmContribution }) => {
+                if (confirmContribution) {
+                  return true;
+                } else {
+                  return false;
+                }
+              },
+            validate: contribution => {
+                if(contribution){
+                    return true;
+                } else{
+                    console.log("Please enter some guidelines for contribution!")
+                }
+            }  
+        },
+        {
+            type: "input",
+            name: "test",
+            message: 'Enter instructions on how to run tests on your project. (Required)',
+            validate: test => {
+                if(test){
+                    return true;
+                } else{
+                    console.log("Please explain how to run tests!")
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Which of the following licenses will you use for your project?',
+            choices: [
+                'apache',
+                'mit',
+                'gnu',
+                'no license'
+            ]
+        }
+        
+    ];
+
+    // TODO: Create a function to write README file
